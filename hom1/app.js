b6482e5eb4d330f463a17461ca7d8f9fcb25fe47
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-06-22 20:22:14 +0300

    lec1

--- hom1/app.js
+++ hom1/app.js
@@ -1,11 +1,11 @@
-const fs=require("fs");
-const path=require("path");
-const path1800=path.join(__dirname,'1800');
-const path2000=path.join(__dirname,'2000');
-const boys=path.join(__dirname,'boys');
-const girls=path.join(__dirname,'girls');
-const Directory=path.join(__dirname,"get-all-files");
-const path2021=path.join(__dirname,"2021");
+const fs = require("fs");
+const path = require("path");
+const path1800 = path.join(__dirname, '1800');
+const path2000 = path.join(__dirname, '2000');
+const boys = path.join(__dirname, 'boys');
+const girls = path.join(__dirname, 'girls');
+const Directory = path.join(__dirname, "get-all-files");
+const path2021 = path.join(__dirname, "2021");
 
 // 1) Студентів з 1800 перевести в групу на 2000. І навпаки
 
@@ -48,28 +48,30 @@ const path2021=path.join(__dirname,"2021");
 //  }
 // findGender(path1800);
 //  * вам потрбіно перемісти всі файлики з вкладених папок в іншу папку. Зробити всі файли на одному рівні вкладеності.
-  function getAllFiles(fromDir){
-        fs.readdir(fromDir,(err, files) => {
-            files.forEach(value => {
-                let newPath=path.join(fromDir,value);
-                fs.stat(newPath,(err1, stats) => {
-                   stats.isDirectory() ? getAllFiles(newPath) : moveToDir(newPath);
-                })
+function getAllFiles(fromDir) {
+    fs.readdir(fromDir, (err, files) => {
+        files.forEach(value => {
+            let newPath = path.join(fromDir, value);
+            fs.stat(newPath, (err1, stats) => {
+                stats.isDirectory() ? getAllFiles(newPath) : moveToDir(newPath);
             })
         })
-  }
-  function moveToDir(Dir){
-     fs.readFile(Dir,(err, data) => {
-         if(err){
-             console.log(err);
-         }
-         let fileName=Dir.split("\\");
-         fileName=fileName[fileName.length-1];
-         fs.writeFile(path.join(Directory,fileName),data.toString(),err1 => {
-            if(err1){
+    })
+}
+
+function moveToDir(Dir) {
+    fs.readFile(Dir, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        let fileName = Dir.split("\\");
+        fileName = fileName[fileName.length - 1];
+        fs.writeFile(path.join(Directory, fileName), data.toString(), err1 => {
+            if (err1) {
                 console.log(err1);
             }
-         })
-     })
-  }
+        })
+    })
+}
+
 getAllFiles(path2021);
