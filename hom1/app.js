462133711ae9113576ab344d35ca1c2014c3ab01
akvasnuk1 <51259882+akvasnuk1@users.noreply.github.com>
2021-06-29 20:35:40 +0300

    change app.js

--- hom1/app.js
+++ hom1/app.js
@@ -9,61 +9,64 @@ const Directory = path.join(__dirname, "get-all-files");
 const path2021 = path.join(__dirname, "2021");
 
 // 1) Студентів з 1800 перевести в групу на 2000. І навпаки
- function moveStudents(srcDir,dstDir){
-     fs.readdir(srcDir,(err, files) => {
-       
-         files.forEach(value => {
-             fs.rename(path.join(srcDir,value),path.join(dstDir,value),err => {
-               
-                 if(err){
-                     console.log(err);
-                 }
-             });
-         })
-     })
- }
-moveStudents(path1800,path2000);
-moveStudents(path2000,path1800);
+function moveStudents(srcDir, dstDir) {
+    fs.readdir(srcDir, (err, files) => {
+
+        files.forEach(value => {
+            fs.rename(path.join(srcDir, value), path.join(dstDir, value), err => {
+
+                if (err) {
+                    console.log(err);
+                }
+            });
+        })
+    })
+}
+
+moveStudents(path1800, path2000);
+moveStudents(path2000, path1800);
 
 // 2) Перемістити всіх дівчат в папку girls а хлопців в папаку boys.
- function findGender(filePath){
-   
-     fs.readdir(filePath,(err, files) => {
-       
-         files.forEach(value => {
-             fs.readFile(path.join(filePath,value),(err, data) => {
-               
-                 const {gender}=JSON.parse(data.toString());
+function findGender(filePath) {
 
-                 if(gender==="female"){
-                     fs.rename(path.join(filePath,value),path.join(girls,value),err =>{
-                       
-                         if(err){
-                             console.log(err);
-                             return err;
-                         }
-                     })
-                 }
-                     return fs.rename(path.join(filePath,value),path.join(boys,value),err => {
-                       
-                         if(err){
-                             console.log(err);
-                         }
-                     })
-             })
-         })
-     })
- }
+    fs.readdir(filePath, (err, files) => {
+
+        files.forEach(value => {
+            fs.readFile(path.join(filePath, value), (err, data) => {
+
+                const {gender} = JSON.parse(data.toString());
+
+                if (gender === "female") {
+                    fs.rename(path.join(filePath, value), path.join(girls, value), err => {
+
+                        if (err) {
+                            console.log(err);
+                            return err;
+                        }
+                    })
+                }
+                return fs.rename(path.join(filePath, value), path.join(boys, value), err => {
+
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            })
+        })
+    })
+}
+
 findGender(path1800);
+
 //  * вам потрбіно перемісти всі файлики з вкладених папок в іншу папку. Зробити всі файли на одному рівні вкладеності.
 function getAllFiles(fromDir) {
     fs.readdir(fromDir, (err, files) => {
-      
+
         files.forEach(value => {
             let newPath = path.join(fromDir, value);
 
             fs.stat(newPath, (err, stats) => {
-                if(err){
+                if (err) {
                     console.log(err);
                 }
                 stats.isDirectory()
@@ -82,10 +85,10 @@ function moveToDir(Dir) {
         let fileName = Dir.split("\\");
         fileName = fileName[fileName.length - 1];
 
-        fs.rename(Dir,path.join(Directory,fileName),err => {
-            if(err){
+        fs.rename(Dir, path.join(Directory, fileName), err => {
+            if (err) {
                 console.log(err);
-              return err;
+                return err;
             }
         })
     })
