691e6835a0c6042061888f6fb9cb5f7020930efd
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-06-29 14:17:09 +0300

    hom4

--- hom4/routes/user.routes.js
+++ hom4/routes/user.routes.js
@@ -5,9 +5,9 @@ const { userMiddleware } = require('../middlewars/index');
 
 router.get('/', userController.allUser);
 
-router.get('/:userId', userMiddleware.isUserExists, userController.getUser);
-
 router.post('/', userMiddleware.isFieldEmpty, userMiddleware.isUserRegister, userController.createUser);
+
+router.get('/:userId', userMiddleware.isUserExists, userController.getUser);
 
 router.delete('/:userId', userMiddleware.isUserExists, userController.deleteUser);
 
