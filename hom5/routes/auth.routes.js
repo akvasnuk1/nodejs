daf62c6b120117d29d5d4066b8be709328bdbb65
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-07-01 18:37:03 +0300

    hom5

--- hom5/routes/auth.routes.js
+++ hom5/routes/auth.routes.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 const { authController } = require('../controllers');
 const { authMiddleware } = require('../middlewars');
 
-router.post('/', authMiddleware.checkIsUserExist, authMiddleware.checkIsPasswordValid, authController.AuthUser);
+router.post('/',
+  authMiddleware.checkIsUserDataValid,
+  authMiddleware.checkIsUserExist,
+  authMiddleware.checkIsPasswordValid,
+  authController.AuthUser);
 
 module.exports = router;
