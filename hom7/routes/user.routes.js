7e2c669d6559d363425e1a6eaf47a324e2485e67
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-07-09 10:55:04 +0300

    hom7

--- hom7/routes/user.routes.js
+++ hom7/routes/user.routes.js
@@ -7,17 +7,19 @@ router.get('/', userController.allUser);
 
 router.post('/', userMiddleware.isUserDataValid, userMiddleware.isUserRegister, userController.createUser);
 
-router.get('/:userId', userMiddleware.isUserExists, userController.getUser);
+router.use('/:userId', userMiddleware.isUserExists);
 
-router.delete('/:userId', userMiddleware.isUserExists, authMiddleware.checkAccessToken, userController.deleteUser);
+router.get('/:userId', userController.getUser);
+
+router.delete('/:userId', authMiddleware.checkAccessToken, userController.deleteUser);
 
 // eslint-disable-next-line max-len
-router.patch('/:userId', userMiddleware.isUserUpdateDataValid, userMiddleware.isUserExists, authMiddleware.checkAccessToken, userController.updateUser);
-
-router.get('/:userId/cars/:status', carController.getUserCarsByStatus);
-
-router.get('/:userId/cars/:carId', carMiddleware.isCarExist, carController.getCar);
+router.patch('/:userId', userMiddleware.isUserUpdateDataValid, authMiddleware.checkAccessToken, userController.updateUser);
 
 router.get('/:userId/cars', carController.getUserCars);
+
+router.get('/:userId/cars/:status', carMiddleware.isCarUpdateDataValid, carController.getUserCarsByStatus);
+
+router.get('/:userId/cars/:carId', carMiddleware.isCarExist, carController.getCar);
 
 module.exports = router;
