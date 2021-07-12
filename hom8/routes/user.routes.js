4788a1f373be9d07db55336d2ade5f52c450306f
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-07-12 14:19:40 +0300

    hom8

--- hom8/routes/user.routes.js
+++ hom8/routes/user.routes.js
@@ -26,10 +26,10 @@ router.get('/:userId/cars/:status', carMiddleware.isCarUpdateDataValid, carContr
 router.get('/:userId/cars/:carId', carMiddleware.isCarExist, carController.getCar);
 
 // eslint-disable-next-line max-len
-router.post('/:userId/loadfiles/:files', authMiddleware.checkAccessToken, fileMiddleware.checkFiles, fileMiddleware.checkFilesPath, userController.addFilesOrRemove);
+router.post('/:userId/loadFiles/:files', authMiddleware.checkAccessToken, fileMiddleware.checkFiles, fileMiddleware.checkFilesPath, userController.addFilesOrRemove);
 
 // eslint-disable-next-line max-len
-router.delete('/:userId/deletefiles/:files', authMiddleware.checkAccessToken, fileMiddleware.checkFilesPath, userController.addFilesOrRemove);
+router.delete('/:userId/deleteFiles/:files', authMiddleware.checkAccessToken, fileMiddleware.checkFilesPath, userController.addFilesOrRemove);
 
 // eslint-disable-next-line max-len
 router.patch('/:userId/avatar/update', authMiddleware.checkAccessToken, fileMiddleware.checkFiles, fileMiddleware.checkAvatar, userController.updateOrDeleteAvatar);
